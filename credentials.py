79b95b3cb29a83211cc4d44f022d2fde091c49b8
Ashwin Joy <32847401+AshwinJoy@users.noreply.github.com>
2018-01-01 17:55:29 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,6 @@
+account_sid='AC40a4a8a76d4c90c378fd358b80703c1f'
+auth_token='fbac3f1c256b9c28ab5b1fe8d834b75b'
+my_cell='+919567974819'
+my_twilio='+18158284645'
+
+ #these informations are obtained from https://twilio.com/user/account
